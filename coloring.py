46e2f30d3f1a8a32434ce96551f364e0c8875dcc
Nir <glazer.nir@campus.technion.ac.il>
2025-01-07 17:19:39 +0200

    initial #100

--- coloring.py
+++ coloring.py
@@ -1,16 +1,34 @@
 import pandas as pd
 import matplotlib
+
+#from performance_vectors import weighted_data
+
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 import numpy as np
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 def plot_dataset_with_continuous_coloring(data, param):
     def on_click(event):
         if event.inaxes:
-            print(f"Clicked point: ({event.xdata}, {event.ydata})")
+            # Get the coordinates of the click
+            x_click, y_click = event.xdata, event.ydata
+
+            # Find the closest point in the dataset
+            distances = np.sqrt((data['PC1'] - x_click) ** 2 + (data['PC2'] - y_click) ** 2)
+            closest_index = distances.idxmin()
+
+            # Get the corresponding PC1, PC2, and player_id values
+            pc1 = data.loc[closest_index, 'PC1']
+            pc2 = data.loc[closest_index, 'PC2']
+            player_id = data.loc[closest_index, 'player_id']
+
+            print(f"Clicked point: PC1={pc1}, PC2={pc2}, player_id={player_id}")
 
     plt.figure(figsize=(14, 10))
     sc = plt.scatter(
@@ -32,17 +50,65 @@ def plot_dataset_with_continuous_coloring(data, param):
     plt.show()
 
 
+# def plot_dataset_with_continuous_coloring(data, param):
+#     def on_click(event):
+#         if event.inaxes:
+#             print(f"Clicked point: ({event.xdata}, {event.ydata})")
+#
+#     plt.figure(figsize=(14, 10))
+#     sc = plt.scatter(
+#         data['PC1'], data['PC2'],
+#         c=data[param],
+#         cmap='Reds',
+#         s=100,
+#         alpha=0.8
+#     )
+#     cbar = plt.colorbar(sc)
+#     cbar.set_label(param)
+#     plt.xlabel("PC1")
+#     plt.ylabel("PC2")
+#     plt.title("Scatter Plot of Points Colored by " + param)
+#     plt.tight_layout()
+#
+#     fig = plt.gcf()
+#     fig.canvas.mpl_connect('button_press_event', on_click)
+#     plt.show()
+#
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 def plot_dataset_with_click_and_coloring(data, param):
     def on_click(event):
         if event.inaxes:
-            print(f"Clicked point: ({event.xdata}, {event.ydata})")
+            # Get the coordinates of the click
+            x_click, y_click = event.xdata, event.ydata
+
+            # Find the closest point in the dataset
+            distances = np.sqrt((data['PC1'] - x_click) ** 2 + (data['PC2'] - y_click) ** 2)
+            closest_index = distances.idxmin()
+
+            # Get the corresponding PC1, PC2, and player_id values
+            pc1 = data.loc[closest_index, 'PC1']
+            pc2 = data.loc[closest_index, 'PC2']
+            player_id = data.loc[closest_index, 'player_id']
+
+            print(f"Clicked point: PC1={pc1}, PC2={pc2}, player_id={player_id}")
 
     plt.figure(figsize=(14, 10))
     param_values = data[param].unique()
     colors = plt.cm.tab10(range(len(param_values)))
     for sub_position, color in zip(param_values, colors):
         subset = data[data[param] == sub_position]
-        plt.scatter(subset['PC1'], subset['PC2'], label=sub_position, color=color, picker=True)
+        plt.scatter(
+            subset['PC1'],
+            subset['PC2'],
+            label=sub_position,
+            color=color,
+            picker=True
+        )
 
     plt.legend(title=param)
     plt.xlabel("PC1")
@@ -53,12 +119,28 @@ def plot_dataset_with_click_and_coloring(data, param):
     fig.canvas.mpl_connect('button_press_event', on_click)
     plt.show()
 
-def get_player_id_by_dimensions(data, pc1, pc2):
-    match = data[(data['PC1'] == pc1) & (data['PC2'] == pc2)]
-    if not match.empty:
-        return match['player_id'].iloc[0]
-    else:
-        return None
+
+# def plot_dataset_with_click_and_coloring(data, param):
+#     def on_click(event):
+#         if event.inaxes:
+#             print(f"Clicked point: ({event.xdata}, {event.ydata})")
+#
+#     plt.figure(figsize=(14, 10))
+#     param_values = data[param].unique()
+#     colors = plt.cm.tab10(range(len(param_values)))
+#     for sub_position, color in zip(param_values, colors):
+#         subset = data[data[param] == sub_position]
+#         plt.scatter(subset['PC1'], subset['PC2'], label=sub_position, color=color, picker=True)
+#
+#     plt.legend(title=param)
+#     plt.xlabel("PC1")
+#     plt.ylabel("PC2")
+#     plt.title("Scatter Plot of Points Colored by " + param)
+#
+#     fig = plt.gcf()  # Get current figure
+#     fig.canvas.mpl_connect('button_press_event', on_click)
+#     plt.show()
+#
 
 
 def coloringByParam(path,param,coloringShape):
@@ -171,16 +253,54 @@ def prepare_definiteTime_stats(path,requestedYear):
     result = df_players_in_2017.merge(w_players, on="player_id", how="left")
     result.to_csv(str(requestedYear) + '_stats.csv', index=False)
 
+
+from prettytable import PrettyTable
+
+
+def head_to_head_comparison(weightedData, playersData, player1_id, player2_id):
+    player1_weighted = weightedData[weightedData['player_id'] == player1_id].iloc[0]
+    player2_weighted = weightedData[weightedData['player_id'] == player2_id].iloc[0]
+    player1_players = \
+    playersData[playersData['player_id'] == player1_id].drop(columns=['image_url', 'url'], errors='ignore').iloc[0]
+    player2_players = \
+    playersData[playersData['player_id'] == player2_id].drop(columns=['image_url', 'url'], errors='ignore').iloc[0]
+
+    table = PrettyTable()
+    table.field_names = ["Statistic", f"Player {player1_id}", f"Player {player2_id}"]
+
+    for column in weightedData.columns.drop('player_id'):
+        table.add_row([column, player1_weighted[column], player2_weighted[column]])
+
+    for column in player1_players.index:
+        table.add_row([column, player1_players[column], player2_players[column]])
+
+    print(table)
+
+# def head_to_head_comparison(weightedData,playersData, player1_id, player2_id):
+#     print(weightedData.head())
+#     player1_data = weightedData[weightedData['player_id'] == player1_id].iloc[0]
+#     player2_data = weightedData[weightedData['player_id'] == player2_id].iloc[0]
+#     columns_to_compare = weightedData.columns.drop('player_id')
+#     table = PrettyTable()
+#     table.field_names = ["Statistic", f"Player {player1_id}", f"Player {player2_id}"]
+#     for column in columns_to_compare:
+#         table.add_row([column, player1_data[column], player2_data[column]])
+#     print(table)
+
+
+
+
 if __name__ == '__main__':
 
-    # categorical coloring:
+    #categorical coloring:
 
-    coloringByParam('datasets/players.csv', 'position',1)
+    #coloringByParam('datasets/players.csv', 'position',1)
+
     # # prepare_League()
     # coloringByParam('categorical_League.csv','current_club_domestic_competition_id',1)
     # # prepare_country()
     # coloringByParam('categorical_country.csv','country_of_birth',1)
-    #coloringByParam('datasets/players.csv','foot',1)
+    # coloringByParam('datasets/players.csv','foot',1)
     #
     # #continuous coloring:
     # coloringByParam('datasets/players.csv','market_value_in_eur',0)
@@ -188,8 +308,12 @@ if __name__ == '__main__':
     # # prepare_definiteTime_stats('datasets/players.csv',2020)
     # # prepare_age(2020,str(2020) + "_stats.csv")
     # coloringByParam('players_age.csv', 'age', 0)
-    #
-    # result = pd.read_csv("pca_results.csv")
-    # print(get_player_id_by_dimensions(result, -0.656672, 1.130110))
+
+    weightedData = pd.read_csv('datasets/weighted_vector_appearances.csv')
+    playersData = pd.read_csv('datasets/players.csv')
+    head_to_head_comparison(weightedData,playersData, 542684,15452)
+
+
+
 
 
